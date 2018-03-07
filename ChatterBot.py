7ed80c814986f6b60e0a6cd096d4bbed8b05894f
Yamini <yamini@github.com>
2018-03-06 21:07:30 -0800

    mod chatter.py

--- ChatterBot.py
+++ ChatterBot.py
@@ -1,19 +1,30 @@
+# Create a function that tweets
 # Dependencies
 import tweepy
 import json
 import time
 
 # Twitter API Keys
-consumer_key = "Ed4RNulN1lp7AbOooHa9STCoU"
-consumer_secret = "P7cUJlmJZq0VaCY0Jg7COliwQqzK0qYEyUF9Y0idx4ujb3ZlW5"
-access_token = "839621358724198402-dzdOsx2WWHrSuBwyNUiqSEnTivHozAZ"
-access_token_secret = "dCZ80uNRbFDjxdU2EckmNiSckdoATach6Q8zb7YYYE5ER"
+import os
+consumer_key = os.getenv("twitter_consumer_key")
+consumer_secret = os.getenv("twitter_consumer_secret")
+access_token = os.getenv("twitter_access_token")
+access_token_secret = os.getenv("twitter_access_token_secret")
 
 # Setup Tweepy API Authentication
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
 
-
 # Create a function that tweets
-# CODE GOES HERE
+def TweetOut(tweet_number):
+    api.update_status(f"practice tweet from python script deployed to heroku: #{tweet_number}!")
+
+# Create a function that calls the TweetOut function every minute
+counter = 0
+
+t_end = time.time() + 60 * 5
+while(time.time() < t_end):
+    TweetOut(counter)
+    time.sleep(60)
+    counter = counter + 1
